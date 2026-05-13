d3c17a6cc1c9968384d4a0e95432ba0c1a22d159
UselessBullets <80850784+UselessBullets@users.noreply.github.com>
2026-05-13 06:23:03 -0500

    inlined http download thread

--- src/main/java/net/minecraft/client/renderer/HttpTexture.java
+++ src/main/java/net/minecraft/client/renderer/HttpTexture.java
@@ -4,7 +4,11 @@
 
 package net.minecraft.client.renderer;
 
+import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
 
 public class HttpTexture
 {
@@ -17,6 +21,29 @@ public class HttpTexture
         this.count = 1;
         this.id = -1;
         this.isLoaded = false;
-        new HttpTexture_DownloadThread(this, url, processor).start();
+        new Thread(() -> {
+            URLConnection urlConnection = null;
+            try {
+                urlConnection = new URL(url).openConnection();
+                urlConnection.setDoInput(true);
+                urlConnection.setDoOutput(false);
+                urlConnection.connect();
+                if (((HttpURLConnection)urlConnection).getResponseCode() / 100 == 4) {
+                    return;
+                }
+                if (processor == null) {
+                    loadedImage = ImageIO.read(urlConnection.getInputStream());
+                }
+                else {
+                    loadedImage = processor.process(ImageIO.read(urlConnection.getInputStream()));
+                }
+            }
+            catch (final Exception ex) {
+                ex.printStackTrace();
+            }
+            finally {
+                ((HttpURLConnection)urlConnection).disconnect();
+            }
+        }).start();
     }
 }

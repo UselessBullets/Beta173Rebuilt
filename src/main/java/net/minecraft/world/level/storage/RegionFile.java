b442f70067af4a7fd0790708b143557d8c7538ea
UselessBullets <80850784+UselessBullets@users.noreply.github.com>
2026-08-26 08:11:20 -0500

    Finished Storage cleanup

--- src/main/java/net/minecraft/world/level/storage/RegionFile.java
+++ src/main/java/net/minecraft/world/level/storage/RegionFile.java
@@ -18,71 +18,80 @@ import java.io.File;
 
 public class RegionFile
 {
-    private static final byte[] emptySection;
+    private static final int VERSION_GZIP = 1;
+    private static final int VERSION_DEFLATE = 2;
+
+    private static final int SECTOR_BYTES = 4096;
+    private static final int SECTOR_INTS = SECTOR_BYTES / 4;
+
+    private static final int CHUNK_HEADER_SIZE = 5; // Useless - was 8 in LCE but usage indicates it should be 5 here
+    private static final byte[] emptySection = new byte[SECTOR_BYTES];
     private final File fileName;
     private RandomAccessFile saveFile;
-    private final int[] offsets;
-    private final int[] chunkTimestamps;
+    private final int[] offsets = new int[SECTOR_INTS];
+    private final int[] chunkTimestamps = new int[SECTOR_INTS];
     private ArrayList<Boolean> sectorFree;
     private int sizeDelta;
-    private long lastModified;
+    private long lastModified = 0L;
     
     public RegionFile(final File path) {
-        this.lastModified = 0L;
-        this.offsets = new int[1024];
-        this.chunkTimestamps = new int[1024];
         this.fileName = path;
+
         this.debugln("REGION LOAD " + this.fileName);
+
         this.sizeDelta = 0;
         try {
             if (path.exists()) {
                 this.lastModified = path.lastModified();
             }
+
             this.saveFile = new RandomAccessFile(path, "rw");
-            if (this.saveFile.length() < 4096L) {
-                for (int i = 0; i < 1024; ++i) {
+            if (this.saveFile.length() < SECTOR_BYTES) {
+                for (int i = 0; i < SECTOR_INTS; ++i) {
                     this.saveFile.writeInt(0);
                 }
-                for (int j = 0; j < 1024; ++j) {
+                for (int i = 0; i < SECTOR_INTS; ++i) {
                     this.saveFile.writeInt(0);
                 }
-                this.sizeDelta += 8192;
+                this.sizeDelta += SECTOR_BYTES * 2;
             }
             if ((this.saveFile.length() & 0xFFFL) != 0x0L) {
-                for (int n = 0; n < (this.saveFile.length() & 0xFFFL); ++n) {
+                for (int i = 0; i < (this.saveFile.length() & 0xFFFL); ++i) {
                     this.saveFile.write(0);
                 }
             }
-            final int initialCapacity = (int)this.saveFile.length() / 4096;
+            final int initialCapacity = (int)this.saveFile.length() / SECTOR_BYTES;
             this.sectorFree = new ArrayList(initialCapacity);
-            for (int k = 0; k < initialCapacity; ++k) {
+            for (int i = 0; i < initialCapacity; ++i) {
                 this.sectorFree.add(true);
             }
-            this.sectorFree.set(0, false);
-            this.sectorFree.set(1, false);
+
+            this.sectorFree.set(0, false); // chunk offset table
+            this.sectorFree.set(1, false); // for the last modified info
+
             this.saveFile.seek(0L);
-            for (int l = 0; l < 1024; ++l) {
-                final int int1 = this.saveFile.readInt();
-                this.offsets[l] = int1;
-                if (int1 != 0 && (int1 >> 8) + (int1 & 0xFF) <= this.sectorFree.size()) {
-                    for (int n2 = 0; n2 < (int1 & 0xFF); ++n2) {
-                        this.sectorFree.set((int1 >> 8) + n2, false);
+            for (int i = 0; i < SECTOR_INTS; ++i) {
+                final int offset = this.saveFile.readInt();
+                this.offsets[i] = offset;
+                if (offset != 0 && (offset >> 8) + (offset & 0xFF) <= this.sectorFree.size()) {
+                    for (int sectorNum = 0; sectorNum < (offset & 0xFF); ++sectorNum) {
+                        this.sectorFree.set((offset >> 8) + sectorNum, false);
                     }
                 }
             }
-            for (int n3 = 0; n3 < 1024; ++n3) {
-                this.chunkTimestamps[n3] = this.saveFile.readInt();
+            for (int i = 0; i < SECTOR_INTS; ++i) {
+                this.chunkTimestamps[i] = this.saveFile.readInt();
             }
         }
-        catch (final IOException ex) {
-            ex.printStackTrace();
+        catch (final IOException e) {
+            e.printStackTrace();
         }
     }
     
     public synchronized int getSizeDelta() {
-        final int sizeDelta = this.sizeDelta;
+        final int ret = this.sizeDelta;
         this.sizeDelta = 0;
-        return sizeDelta;
+        return ret;
     }
     
     private void debug(final String msg) {
@@ -109,125 +118,147 @@ public class RegionFile
             this.debugln("READ", x, z, "out of bounds");
             return null;
         }
+
         try {
             final int offset = this.getOffset(x, z);
             if (offset == 0) {
                 return null;
             }
-            final int n = offset >> 8;
-            final int i = offset & 0xFF;
-            if (n + i > this.sectorFree.size()) {
+
+            final int sectorNumber = offset >> 8;
+            final int numSectors = offset & 0xFF;
+
+            if (sectorNumber + numSectors > this.sectorFree.size()) {
                 this.debugln("READ", x, z, "invalid sector");
                 return null;
             }
-            this.saveFile.seek(n * 4096);
-            final int int1 = this.saveFile.readInt();
-            if (int1 > 4096 * i) {
-                this.debugln("READ", x, z, "invalid length: " + int1 + " > 4096 * " + i);
+
+            this.saveFile.seek(sectorNumber * SECTOR_BYTES);
+
+            final int length = this.saveFile.readInt();
+            if (length > SECTOR_BYTES * numSectors) {
+                this.debugln("READ", x, z, "invalid length: " + length + " > 4096 * " + numSectors);
                 return null;
             }
-            final byte byte1 = this.saveFile.readByte();
-            if (byte1 == 1) {
-                final byte[] array = new byte[int1 - 1];
+
+            final byte version = this.saveFile.readByte();
+            if (version == VERSION_GZIP) {
+                final byte[] array = new byte[length - 1];
                 this.saveFile.read(array);
                 return new DataInputStream(new GZIPInputStream(new ByteArrayInputStream(array)));
             }
-            if (byte1 == 2) {
-                final byte[] array2 = new byte[int1 - 1];
+
+            if (version == VERSION_DEFLATE) {
+                final byte[] array2 = new byte[length - 1];
                 this.saveFile.read(array2);
                 return new DataInputStream(new InflaterInputStream(new ByteArrayInputStream(array2)));
             }
-            this.debugln("READ", x, z, "unknown version " + byte1);
+
+            this.debugln("READ", x, z, "unknown version " + version);
             return null;
         }
-        catch (final IOException ex) {
+        catch (final IOException e) {
             this.debugln("READ", x, z, "exception");
             return null;
         }
     }
     
     public DataOutputStream getChunkDataOutputStream(final int x, final int z) {
-        if (this.outOfBounds(x, z)) {
-            return null;
-        }
+        if (this.outOfBounds(x, z)) return null;
         return new DataOutputStream(new DeflaterOutputStream(new ChunkBuffer(this, x, z)));
     }
     
     protected synchronized void write(final int x, final int y, final byte[] data, final int length) {
         try {
-            final int offset = this.getOffset(x, y);
-            final int sectorNumber = offset >> 8;
-            final int n = offset & 0xFF;
-            final int n2 = (length + 5) / 4096 + 1;
-            if (n2 >= 256) {
+            int offset = this.getOffset(x, y);
+            int sectorNumber = offset >> 8;
+            int sectorsAllocated = offset & 0xFF;
+
+            int sectorsNeeded = (length + CHUNK_HEADER_SIZE) / SECTOR_BYTES + 1;
+
+            // maximum chunk size is 1MB
+            if (sectorsNeeded >= 256) {
                 return;
             }
-            if (sectorNumber != 0 && n == n2) {
+
+            if (sectorNumber != 0 && sectorsAllocated == sectorsNeeded) {
+                /* we can simply overwrite the old sectors */
                 this.debug("SAVE", x, y, length, "rewrite");
                 this.write(sectorNumber, data, length);
             }
             else {
-                for (int i = 0; i < n; ++i) {
+                /* we need to allocate new sectors */
+
+                /* mark the sectors previously used for this chunk as free */
+                for (int i = 0; i < sectorsAllocated; ++i) {
                     this.sectorFree.set(sectorNumber + i, true);
                 }
-                int index = this.sectorFree.indexOf(true);
-                int n3 = 0;
-                if (index != -1) {
-                    for (int j = index; j < this.sectorFree.size(); ++j) {
-                        if (n3 != 0) {
-                            if (this.sectorFree.get(j)) {
-                                ++n3;
-                            }
-                            else {
-                                n3 = 0;
-                            }
+
+                /* scan for a free space large enough to store this chunk */
+                int runStart = this.sectorFree.indexOf(true);
+                int runLength = 0;
+                if (runStart != -1) {
+                    for (int i = runStart; i < this.sectorFree.size(); ++i) {
+                        if (runLength != 0) {
+                            if (this.sectorFree.get(i)) runLength++;
+                            else runLength = 0;
                         }
-                        else if (this.sectorFree.get(j)) {
-                            index = j;
-                            n3 = 1;
+                        else if (this.sectorFree.get(i)) {
+                            runStart = i;
+                            runLength = 1;
                         }
-                        if (n3 >= n2) {
+
+                        if (runLength >= sectorsNeeded) {
                             break;
                         }
                     }
                 }
-                if (n3 >= n2) {
+
+                if (runLength >= sectorsNeeded) {
+                    /* we found a free space large enough */
                     this.debug("SAVE", x, y, length, "reuse");
-                    final int sectorNumber2 = index;
-                    this.setOffset(x, y, sectorNumber2 << 8 | n2);
-                    for (int k = 0; k < n2; ++k) {
-                        this.sectorFree.set(sectorNumber2 + k, false);
+                    sectorNumber = runStart;
+                    this.setOffset(x, y, sectorNumber << 8 | sectorsNeeded);
+                    for (int i = 0; i < sectorsNeeded; ++i) {
+                        this.sectorFree.set(sectorNumber + i, false);
                     }
-                    this.write(sectorNumber2, data, length);
+                    this.write(sectorNumber, data, length);
                 }
                 else {
+                    /*
+                     * no free space large enough found -- we need to grow the
+                     * file
+                     */
                     this.debug("SAVE", x, y, length, "grow");
                     this.saveFile.seek(this.saveFile.length());
                     final int size = this.sectorFree.size();
-                    for (int l = 0; l < n2; ++l) {
+                    for (int i = 0; i < sectorsNeeded; ++i) {
                         this.saveFile.write(RegionFile.emptySection);
                         this.sectorFree.add(false);
                     }
-                    this.sizeDelta += 4096 * n2;
+                    this.sizeDelta += SECTOR_BYTES * sectorsNeeded;
+
                     this.write(size, data, length);
-                    this.setOffset(x, y, size << 8 | n2);
+                    this.setOffset(x, y, size << 8 | sectorsNeeded);
                 }
             }
             this.setTimestamp(x, y, (int)(System.currentTimeMillis() / 1000L));
         }
-        catch (final IOException ex) {
-            ex.printStackTrace();
+        catch (final IOException e) {
+            e.printStackTrace();
         }
     }
-    
+
+    /* write a chunk data to the region file at specified sector number */
     private void write(final int sectorNumber, final byte[] data, final int length) throws IOException {
         this.debugln(" " + sectorNumber);
-        this.saveFile.seek(sectorNumber * 4096);
+        this.saveFile.seek(sectorNumber * SECTOR_BYTES);
         this.saveFile.writeInt(length + 1);
         this.saveFile.writeByte(2);
         this.saveFile.write(data, 0, length);
     }
-    
+
+    /* is this an invalid chunk coordinate? */
     private boolean outOfBounds(final int x, final int z) {
         return x < 0 || x >= 32 || z < 0 || z >= 32;
     }
@@ -248,16 +279,12 @@ public class RegionFile
     
     private void setTimestamp(final int x, final int z, final int value) throws IOException {
         this.chunkTimestamps[x + z * 32] = value;
-        this.saveFile.seek(4096 + (x + z * 32) * 4);
+        this.saveFile.seek(SECTOR_BYTES + (x + z * 32) * 4);
         this.saveFile.writeInt(value);
     }
     
     public void close() throws IOException {
         this.saveFile.close();
-    }
-    
-    static {
-        emptySection = new byte[4096];
     }
 
     static class ChunkBuffer extends ByteArrayOutputStream
--- src/main/java/net/minecraft/world/level/storage/RegionFileCache.java
+++ src/main/java/net/minecraft/world/level/storage/RegionFileCache.java
@@ -15,61 +15,64 @@ import java.util.Map;
 
 public class RegionFileCache
 {
-    private static final Map<File, Reference<RegionFile>> defaultCache;
+    private static final int MAX_CACHE_SIZE = 256;
+    private static final Map<File, Reference<RegionFile>> defaultCache = new HashMap<>();
     
     private RegionFileCache() {
     }
     
     public static synchronized RegionFile getRegionFile(final File saveFile, final int chunkX, final int chunkZ) {
-        final File parent = new File(saveFile, "region");
-        final File path = new File(parent, "r." + (chunkX >> 5) + "." + (chunkZ >> 5) + ".mcr");
-        final Reference<RegionFile> reference = RegionFileCache.defaultCache.get(path);
-        if (reference != null) {
-            final RegionFile regionFile = reference.get();
-            if (regionFile != null) {
-                return regionFile;
-            }
+        final File regionDir = new File(saveFile, "region");
+
+        final File file = new File(regionDir, "r." + (chunkX >> 5) + "." + (chunkZ >> 5) + ".mcr");
+        final Reference<RegionFile> ref = RegionFileCache.defaultCache.get(file);
+        if (ref != null) {
+            final RegionFile regionFile = ref.get();
+            if (regionFile != null) return regionFile;
         }
-        if (!parent.exists()) {
-            parent.mkdirs();
+
+        if (!regionDir.exists()) {
+            regionDir.mkdirs();
         }
-        if (RegionFileCache.defaultCache.size() >= 256) {
+
+        if (RegionFileCache.defaultCache.size() >= MAX_CACHE_SIZE) {
             clear();
         }
-        final RegionFile referent = new RegionFile(path);
-        RegionFileCache.defaultCache.put(path, new SoftReference<>(referent));
-        return referent;
+
+        final RegionFile reg = new RegionFile(file);
+        RegionFileCache.defaultCache.put(file, new SoftReference<>(reg));
+        return reg;
     }
     
     public static synchronized void clear() {
         for (final Reference<RegionFile> reference : RegionFileCache.defaultCache.values()) {
             try {
                 final RegionFile regionFile = reference.get();
-                if (regionFile == null) {
-                    continue;
+                if (regionFile != null) {
+                    regionFile.close();
                 }
-                regionFile.close();
             }
-            catch (final IOException ex) {
-                ex.printStackTrace();
+            catch (final IOException e) {
+                e.printStackTrace();
             }
         }
+
         RegionFileCache.defaultCache.clear();
     }
     
     public static int getSizeDelta(final File saveFile, final int chunkX, final int chunkZ) {
-        return getRegionFile(saveFile, chunkX, chunkZ).getSizeDelta();
+        RegionFile r = getRegionFile(saveFile, chunkX, chunkZ);
+        return r.getSizeDelta();
     }
     
     public static DataInputStream getChunkDataInputStream(final File saveFile, final int chunkX, final int chunkZ) {
-        return getRegionFile(saveFile, chunkX, chunkZ).getChunkDataInputStream(chunkX & 0x1F, chunkZ & 0x1F);
+        RegionFile r = getRegionFile(saveFile, chunkX, chunkZ);
+        return r.getChunkDataInputStream(chunkX & 0x1F, chunkZ & 0x1F);
     }
     
     public static DataOutputStream getChunkDataOutputStream(final File saveFile, final int chunkX, final int chunkZ) {
-        return getRegionFile(saveFile, chunkX, chunkZ).getChunkDataOutputStream(chunkX & 0x1F, chunkZ & 0x1F);
+        RegionFile r = getRegionFile(saveFile, chunkX, chunkZ);
+        return r.getChunkDataOutputStream(chunkX & 0x1F, chunkZ & 0x1F);
     }
-    
-    static {
-        defaultCache = new HashMap<>();
-    }
+
 }
